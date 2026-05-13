daccb8d6dd1fbe64782df40020db3e6bf932970f
juanpe23 <142370092+JuanLBEnjoyer@users.noreply.github.com>
2026-05-13 16:18:02 -0500

    Configuracion inicial del front: - Creacion de componentes principales de la aplicacion - Se agregan las respectivas rutas en app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './component/inicio/inicio';
+import { Registro } from './component/registro/registro';
+import { Login } from './component/login/login';
+import { Navbar } from './component/navbar/navbar';
+import { Perfil } from './component/perfil/perfil';
+import { Solicitudes } from './component/solicitudes/solicitudes';
+import { CrearSolicitud } from './component/crear-solicitud/crear-solicitud';
+import { DetalleSolicitud } from './component/detalle-solicitud/detalle-solicitud';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Inicio },
+  { path: 'registro', component: Registro },
+  { path: 'login', component: Login },
+  { path: '**', pathMatch: 'full', redirectTo: '/' },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('frontProyecto');
+
+  footer = signal('Universidad del Quindio - INGEsis - 2026');
 }

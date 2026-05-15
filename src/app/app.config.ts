4e3c3a6505551b63bdb56764e50b36fc14361e05
juanpe23 <142370092+JuanLBEnjoyer@users.noreply.github.com>
2026-05-15 09:49:31 -0500

    configuracion para implementacion de primeng21 y primeIcons

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,22 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import { Aura } from 'primeng/themes/aura';
 import { routes } from './app.routes';
 import { authInterceptor } from './servicios/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor]))
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: { darkModeSelector: '.app-dark' }
+      }
+    })
   ]
 };
